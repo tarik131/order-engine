8fc05c0ef7a7489544e0437d9bc2fadca21ae7f5
VenuSomarapu3 <142634057+VenuSomarapu3@users.noreply.github.com>
2024-09-27 10:54:47 -0400

    Update Trade.java

--- src/main/java/com/test/model/Trade.java
+++ src/main/java/com/test/model/Trade.java
@@ -1,20 +1,74 @@
 package com.test.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Trade {
     private String id;
     private String ticker;
-    private String trader;
     private Integer quantity;
-    private String buyer;
-    private String seller;
+    private String buyerOrderId;
+    private String sellOrderId;
     private Double price;
-    private String bidOrderId;
-    private String offerOrderId;
+
+    public Trade(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getBuyerOrderId() {
+        return buyerOrderId;
+    }
+
+    public void setBuyerOrderId(String buyerOrderId) {
+        this.buyerOrderId = buyerOrderId;
+    }
+
+    public String getSellOrderId() {
+        return sellOrderId;
+    }
+
+    public void setSellOrderId(String sellOrderId) {
+        this.sellOrderId = sellOrderId;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Trade{" +
+                "id='" + id + '\'' +
+                ", ticker='" + ticker + '\'' +
+                ", quantity=" + quantity +
+                ", buyerOrderId='" + buyerOrderId + '\'' +
+                ", sellOrderId='" + sellOrderId + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
