f8ce7a806c78604469d6a31e970aec3855d9f3db
VenuSomarapu3 <142634057+VenuSomarapu3@users.noreply.github.com>
2024-09-27 10:55:25 -0400

    Update OrderServiceImpl.java

--- src/main/java/com/test/service/OrderServiceImpl.java
+++ src/main/java/com/test/service/OrderServiceImpl.java
@@ -1,12 +1,43 @@
 package com.test.service;
 
 import com.test.model.BondOrder;
+import com.test.model.Trade;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+
+    //Store orders in java data structure(s)
     public boolean submitOrder(BondOrder order){
         return true;
+    }
+
+    //Implement process order
+    private void processOrder(BondOrder order){
+
+        BondOrder buyOrder = null;
+        BondOrder sellOrder = null;
+        int quantity = 0;
+
+        //matching logic
+
+        Trade trade = createTrade(buyOrder,sellOrder,quantity);
+        System.out.println("New Trade happened: "+trade.toString());
+
+    }
+
+
+    private Trade createTrade(BondOrder buyOrder, BondOrder sellOrder,int quantity){
+
+        Trade trade = new Trade();
+
+        trade.setId(buyOrder.getId()+"-"+sellOrder.getId());
+        trade.setTicker(sellOrder.getTicker());
+        trade.setPrice(sellOrder.getPrice());
+        trade.setQuantity(quantity);
+
+        trade.setBuyerOrderId(buyOrder.getId());
+        return trade;
     }
 
 }
